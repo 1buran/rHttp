db3c63ceafe90cfbd4f5a6d428a625dd95631bdc
Andrew Burdyug <buran83@gmail.com>
2024-03-11 22:09:17 +0300

    Add field values, fix form

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	// "fmt"
 	"log"
-	// "net/http"
-	"regexp"
+	"net/http"
+	"net/textproto"
 	"slices"
 	"strings"
-	// "github.com/charmbracelet/bubbles/cursor"
+
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
+// Number of input or ENUM(input type, int)
 const (
 	header int = iota
+	headerVal
 	param
+	paramVal
 	cookie
+	cookieVal
+
+	// the last one is the max index of defined constants
+	fieldsCount
 )
 
 const (
@@ -39,61 +45,29 @@ type orderedKeyVal struct {
 	KeyVal
 }
 
+func (kv *orderedKeyVal) setKey(i *textinput.Model) {
+	k := i.Value()
+	if k != "" {
+		_, ok := kv.KeyVal[k]
+		if !ok {
+			kv.order = append(kv.order, k)
+			kv.KeyVal[k] = ""
+		}
+	}
+}
+
+func (kv *orderedKeyVal) setValue(i1, i2 *textinput.Model) {
+	kv.KeyVal[i1.Value()] = i2.Value()
+	i1.Reset()
+	i2.Reset()
+}
+
 type request struct {
 	method  string
 	uri     string
 	headers orderedKeyVal
-	params  KeyVal
-	cookies KeyVal
-}
-
-func NewOrderedKeVal() orderedKeyVal {
-	return orderedKeyVal{order: []string{}, KeyVal: make(KeyVal)}
-}
-
-func headerValidator(s string) error {
-	// TODO add header validation
-	// https://developers.cloudflare.com/rules/transform/request-header-modification/reference/header-format/
-	// 	The name of the HTTP request header you want to set or remove can only contain:
-
-	// Alphanumeric characters: a-z, A-Z, and 0-9
-	// The following special characters: - and _
-	// The value of the HTTP request header you want to set can only contain:
-
-	// Alphanumeric characters: a-z, A-Z, and 0-9
-	// The following special characters: _ :;.,\/"'?!(){}[]@<>=-+*#$&`|~^%
-	return nil
-}
-
-func paramValidator(s string) error  { return nil }
-func cookieValidator(s string) error { return nil }
-
-func (r *request) AddHeader(k, v string) {
-	// TODO use regexp instead
-	k = strings.Trim(k, " \n\t-")
-	k = strings.ReplaceAll(k, " ", "-")
-
-	re := regexp.MustCompile(`\b(\w)`)
-	k = re.ReplaceAllStringFunc(k, func(s string) string { return strings.ToUpper(s) })
-	if k != "" {
-		// TODO check if already setted
-		r.headers.KeyVal[k] = v
-		r.headers.order = append(r.headers.order, k)
-	}
-}
-
-func (r *request) AddParam(k, v string) {
-	// TODO add param validation
-	if k != "" {
-		r.params[k] = v
-	}
-}
-
-func (r *request) AddCookie(k, v string) {
-	// TODO add param validation
-	if k != "" {
-		r.cookies[k] = v
-	}
+	params  orderedKeyVal
+	cookies orderedKeyVal
 }
 
 func (r request) String() string {
@@ -103,8 +77,21 @@ func (r request) String() string {
 	b.WriteString(uriStyle.Render(r.method+" "+r.uri) + "\n")
 	slices.Sort(r.headers.order)
 	for _, k := range r.headers.order {
-		// TODO Set-Cookie: ...
 		v := r.headers.KeyVal[k]
+		b.WriteString(headerStyle.Render(k+": "+v) + "\n")
+	}
+
+	// TODO add prams to URL query string: ...
+	b.WriteString("params:" + "\n")
+	for _, k := range r.params.order {
+		v := r.params.KeyVal[k]
+		b.WriteString(headerStyle.Render(k+": "+v) + "\n")
+	}
+
+	// TODO Set-Cookie: ...
+	b.WriteString("cookies:" + "\n")
+	for _, k := range r.cookies.order {
+		v := r.cookies.KeyVal[k]
 		b.WriteString(headerStyle.Render(k+": "+v) + "\n")
 	}
 
@@ -113,22 +100,31 @@ func (r request) String() string {
 
 func NewReq() *request {
 	return &request{
-		"GET",
-		"http://example.com",
-		NewOrderedKeVal(),
-		make(KeyVal),
-		make(KeyVal),
-	}
+		"GET", "http://example.com", NewOrderedKeVal(), NewOrderedKeVal(), NewOrderedKeVal()}
+}
+func NewOrderedKeVal() orderedKeyVal {
+	return orderedKeyVal{order: []string{}, KeyVal: make(KeyVal)}
+}
+
+func correctHeader(i *textinput.Model) {
+	// TODO use regexp instead
+	h := i.Value()
+	h = textproto.TrimString(h)
+	h = strings.ReplaceAll(h, " ", "-")
+	h = http.CanonicalHeaderKey(h)
+
+	// auto correct header name
+	i.SetValue(h)
 }
 
 type model struct {
-	req     *request
-	actions []string
-	cursor  int
+	req *request
 	// TODO add inputs for values
-	inputs   []textinput.Model
-	focused  int
-	hideMenu bool
+	inputs    []textinput.Model
+	cursorIdx int    // edit type
+	cursorKey string // edit key of type orderedKeyVal store
+	focused   int
+	hideMenu  bool
 }
 
 // nextInput focuses the next input field
@@ -145,39 +141,53 @@ func (m *model) prevInput() {
 	}
 }
 
+func headerValidator(s string) error {
+	// TODO add header validation
+	// https://developers.cloudflare.com/rules/transform/request-header-modification/reference/header-format/
+	// 	The name of the HTTP request header you want to set or remove can only contain:
+
+	// Alphanumeric characters: a-z, A-Z, and 0-9
+	// The following special characters: - and _
+	// The value of the HTTP request header you want to set can only contain:
+
+	// Alphanumeric characters: a-z, A-Z, and 0-9
+	// The following special characters: _ :;.,\/"'?!(){}[]@<>=-+*#$&`|~^%
+	return nil
+}
+
+func paramValidator(s string) error   { return nil }
+func cookieValidator(s string) error  { return nil }
+func defaultvalidator(s string) error { return nil }
+
+var validators = [fieldsCount]func(string) error{
+	// field name input and field value input validators
+	headerValidator, defaultvalidator,
+	paramValidator, defaultvalidator,
+	cookieValidator, defaultvalidator,
+}
+var prompts = [fieldsCount]string{"Header ", "", "Param  ", "", "Cookie ", ""}
+var placeholders = [fieldsCount]string{"X-Auth-Token", "token value", "products_id", "10", "XDEBUG_SESSION", "debugger"}
+
+func NewKeyValInputs(n int) textinput.Model {
+	t := textinput.New()
+	t.Prompt = prompts[n]
+	t.Placeholder = placeholders[n]
+	t.Width = 30
+	t.TextStyle = textStyle
+	t.PromptStyle = promptStyle
+	t.Validate = validators[n]
+	t.PlaceholderStyle = continueStyle
+	return t
+}
+
 func initialModel() model {
-	var inputs []textinput.Model = make([]textinput.Model, 3)
-	inputs[header] = textinput.New()
-	inputs[header].Placeholder = "X-Custom-Header-Name"
-	inputs[header].Prompt = "Header "
-	inputs[header].Width = 30
-	inputs[header].TextStyle = textStyle
-	inputs[header].PromptStyle = promptStyle
-	inputs[header].Validate = headerValidator
-	inputs[header].PlaceholderStyle = continueStyle
-
-	inputs[param] = textinput.New()
-	inputs[param].Placeholder = "products_id"
-	inputs[param].Prompt = "Param  "
-	inputs[param].Width = 30
-	inputs[param].TextStyle = textStyle
-	inputs[param].PromptStyle = promptStyle
-	inputs[param].Validate = paramValidator
-	inputs[param].PlaceholderStyle = continueStyle
-
-	inputs[cookie] = textinput.New()
-	inputs[cookie].Placeholder = "XDEBUG_SESSION"
-	inputs[cookie].Prompt = "Cookie "
-	inputs[cookie].Width = 30
-	inputs[cookie].TextStyle = textStyle
-	inputs[cookie].PromptStyle = promptStyle
-	inputs[cookie].Validate = cookieValidator
-	inputs[cookie].PlaceholderStyle = continueStyle
-
+	var inputs []textinput.Model
+	for i := 0; i < fieldsCount; i++ {
+		inputs = append(inputs, NewKeyValInputs(i))
+	}
 	return model{
-		actions: []string{"add header", "add query string param", "add cookie"},
-		req:     NewReq(),
-		inputs:  inputs,
+		req:    NewReq(),
+		inputs: inputs,
 	}
 }
 
@@ -198,11 +208,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyCtrlC:
 			return m, tea.Quit
 		case tea.KeyEnter:
-			m.req.AddHeader(m.inputs[m.focused].Value(), "Val")
-			m.inputs[m.focused].Reset()
-			// if m.focused == len(m.inputs)-1 {
-			// 	return m, tea.Quit
-			// }
+			switch m.focused {
+			case header:
+				correctHeader(&m.inputs[header])
+				m.req.headers.setKey(&m.inputs[header])
+			case param:
+				m.req.params.setKey(&m.inputs[param])
+			case cookie:
+				m.req.cookies.setKey(&m.inputs[cookie])
+			case headerVal:
+				m.req.headers.setValue(&m.inputs[header], &m.inputs[headerVal])
+			case paramVal:
+				m.req.params.setValue(&m.inputs[param], &m.inputs[paramVal])
+			case cookieVal:
+				m.req.cookies.setValue(&m.inputs[cookie], &m.inputs[cookieVal])
+			}
+
 			m.nextInput()
 		}
 		for i := 0; i < len(m.inputs); i++ {
@@ -225,9 +246,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m model) View() string {
 	var b strings.Builder
 	b.WriteString("Modify http request:\n")
-	b.WriteString(m.inputs[header].View() + "\n")
-	b.WriteString(m.inputs[param].View() + "\n")
-	b.WriteString(m.inputs[cookie].View() + "\n")
+
+	for i := 0; i < len(m.inputs); i += 2 {
+		b.WriteString(m.inputs[i].View() + " " + m.inputs[i+1].View() + "\n")
+	}
+
 	b.WriteString("\n" + m.req.String())
 	return b.String()
 }
