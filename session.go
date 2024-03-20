e7988b8df6d8e2f49890dd15591fc195040cde9c
Andrew Burdyug <buran83@gmail.com>
2024-03-20 16:06:42 +0300

    Add low level functionality of sessions

---
+++ session.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// Request reflects the [http.Request] params.
+type Request struct {
+	Host       string              `json:"host"`
+	Proto      string              `json:"proto"`
+	Method     string              `json:"method"`
+	UrlPath    string              `json:"url"`
+	Headers    map[string][]string `json:"headers"`
+	FormValues map[string][]string `json:"form"`
+	RawQuery   string              `json:"qs"`
+}
+
+// Response reflects the [http.Response] data.
+type Response struct {
+	Status    string              `json:"status"`
+	Proto     string              `json:"proto"`
+	Headers   map[string][]string `json:"headers"`
+	BodyLines []string            `json:"body"`
+}
+
+// Session reflect current state: some stats, request settings and last response (with data).
+type Session struct {
+	ReqCount int      `json:"reqCount"`
+	ReqTime  int64    `json:"reqTime"`
+	Request  Request  `json:"req"`
+	Response Response `json:"res"`
+}
+
+// Create a new session.
+func NewSession(rq *http.Request, rs *http.Response, rqc int, rqt int64, rqf map[string][]string, rsb []string) (*Session, error) {
+	req := Request{Headers: make(map[string][]string), FormValues: make(map[string][]string)}
+	res := Response{Headers: make(map[string][]string)}
+
+	// populate Request with [http.Request] data
+	if rq != nil {
+		req.Proto = rq.Proto
+		req.Method = rq.Method
+		req.Host = rq.URL.Host
+		req.UrlPath = rq.URL.Path
+		for k, v := range rq.Header {
+			req.Headers[k] = v
+		}
+		req.RawQuery = rq.URL.RawQuery
+		req.FormValues = rqf
+	}
+
+	// populate Response with [http.Response] data
+
+	if rs != nil {
+		res.Status = rs.Status
+		res.Proto = rs.Proto
+		for k, v := range rs.Header {
+			res.Headers[k] = v
+		}
+		res.BodyLines = rsb
+	}
+	return &Session{ReqCount: rqc, ReqTime: rqt, Request: req, Response: res}, nil
+}
+
+// Save the session.
+func (s *Session) Save(o io.Writer) error {
+	b, err := json.Marshal(s)
+	if err != nil {
+		return err
+	}
+
+	_, err = o.Write(b)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Load the session.
+func (s *Session) Load(r io.Reader) error {
+
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(b, s)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ session_test.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"net/http"
+	"net/url"
+	"slices"
+	"strings"
+	"testing"
+)
+
+func TestSession(t *testing.T) {
+	var rq *http.Request
+	var rs *http.Response
+
+	rqf := map[string][]string{}
+	rsb := []string{}
+
+	t.Run("create new session with empty request and response", func(t *testing.T) {
+		_, err := NewSession(rq, rs, 10, 47, rqf, rsb)
+		if err != nil {
+			t.Fatalf("cannot create new session, error: %s", err)
+		}
+	})
+
+	rq = &http.Request{
+		Proto:  "HTTP/1.1",
+		Method: "GET",
+		URL: &url.URL{
+			Host:     "localhost",
+			Path:     "/api/endpoint",
+			RawQuery: "order_by=name&limit=10",
+		},
+		Header: make(http.Header),
+	}
+	rq.Header.Set("content-type", "application/json")
+	rq.Header.Set("auth-token", "token")
+
+	rs = &http.Response{
+		Status: "200 OK",
+		Proto:  "HTTP/1.1",
+		Header: make(http.Header),
+	}
+	rs.Header.Set("connection", "keep-alive")
+	rs.Header.Set("content-length", "34")
+	rs.Header.Set("content-type", "application/json; charset=utf-8")
+	rs.Header.Set("date", "Date:  Wed, 20 Mar 2024 09:11:23 GMT")
+
+	rsb = []string{"{", `"msg": "hello!",`, `"id": 455`, "}"}
+
+	t.Run("create new session filled request and response", func(t *testing.T) {
+		s, err := NewSession(rq, rs, 10, 47, rqf, rsb)
+		if err != nil {
+			t.Fatalf("cannot create new session, error: %s", err)
+		}
+		if s.ReqCount != 10 {
+			t.Errorf("expected req count: 10, got: %d", s.ReqCount)
+		}
+		if s.Request.Proto != "HTTP/1.1" {
+			t.Errorf("expected req proto HTTP/1.1, got: %s", s.Request.Proto)
+		}
+		if s.Response.Status != "200 OK" {
+			t.Errorf("expected res status: 200 OK, got: %s", s.Response.Status)
+		}
+		v, ok := s.Response.Headers["Content-Type"]
+		if !ok || slices.Compare(v, []string{"application/json; charset=utf-8"}) != 0 {
+			t.Errorf(
+				`expected: res header Content-Type="application/json; charset=utf-8", got: %#v`, v,
+			)
+		}
+		if slices.Compare(s.Response.BodyLines, rsb) != 0 {
+			t.Errorf("expected res body lines %s != %s", rsb, s.Response.BodyLines)
+		}
+
+	})
+
+	var buf strings.Builder
+	t.Run("save", func(t *testing.T) {
+		s, _ := NewSession(rq, rs, 10, 47, rqf, rsb)
+
+		err := s.Save(&buf)
+		if err != nil {
+			t.Errorf("there is an error during the Session.Save(): %s", err)
+		}
+
+		if !strings.Contains(
+			buf.String(), `"url":"/api/endpoint","headers":{"Auth-Token":["token"]`) {
+			t.Errorf("not found expected data, saved: %s", buf.String())
+		}
+	})
+
+	t.Run("load", func(t *testing.T) {
+		var rq *http.Request
+		var rs *http.Response
+
+		rqf := map[string][]string{}
+		rsb := []string{"{", `"msg": "hello!",`, `"id": 455`, "}"}
+
+		s, _ := NewSession(rq, rs, 10, 47, rqf, []string{})
+		r := strings.NewReader(buf.String())
+
+		err := s.Load(r)
+		if err != nil {
+			t.Errorf("there is an error during the Session.Load(): %s", err)
+		}
+
+		if s.ReqCount != 10 {
+			t.Errorf("expected req count: 10, got: %d", s.ReqCount)
+		}
+		if s.Request.Proto != "HTTP/1.1" {
+			t.Errorf("expected req proto HTTP/1.1, got: %s", s.Request.Proto)
+		}
+		if s.Response.Status != "200 OK" {
+			t.Errorf("expected res status: 200 OK, got: %s", s.Response.Status)
+		}
+		v, ok := s.Response.Headers["Content-Type"]
+		if !ok || slices.Compare(v, []string{"application/json; charset=utf-8"}) != 0 {
+			t.Errorf(
+				`expected: res header Content-Type="application/json; charset=utf-8", got: %#v`, v,
+			)
+		}
+		if slices.Compare(s.Response.BodyLines, rsb) != 0 {
+			t.Errorf("expected res body lines %s != %s", rsb, s.Response.BodyLines)
+		}
+	})
+}
